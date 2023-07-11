701df8bfdc65355921dfca22c91cef0227eb789a
sauravbaldwin <41564194+sauravbaldwin@users.noreply.github.com>
2023-07-11 16:25:20 +0530

    Add files via upload

---
+++ KeyVault/variables.tf
@@ -0,0 +1,36 @@
+variable "akvname" {
+    type = string
+    description = "Name of the KeyVault"
+  
+}
+
+variable "location" {
+    type = string
+    description = "Location of the NSG or other Resource"
+  
+}
+
+variable "rgname" {
+    type = string
+    description = "ResourceGroup Name where NSG will reside"
+  
+}
+
+variable "akvskuname" {
+    type = string
+    description = "ResourceGroup Name where NSG will reside"
+  
+}
+
+variable "tenantid" {
+    type = string
+    description = "Name of the KeyVault"
+  
+}
+
+variable "objectid" {
+    type = string
+    description = "Name of the KeyVault"
+  
+}
+
