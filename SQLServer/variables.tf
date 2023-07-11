3dbc3dbcbd3ceb6943a0436dafbc07284621b507
sauravbaldwin <41564194+sauravbaldwin@users.noreply.github.com>
2023-07-11 16:30:40 +0530

    Add files via upload

---
+++ SQLServer/variables.tf
@@ -0,0 +1,45 @@
+variable "sqlsername" {
+    type = string
+    description = "Name of the SQL server"
+  
+}
+
+
+variable "rgname" {
+    type = string
+    description = "Resource Group where SQL Server will get deployed"
+  
+}
+
+
+variable "location" {
+  type = string
+  description = "Location of the SQL Server or other resources"
+}
+
+
+variable "adminlogin" {
+    type = string
+    description = "Resource Group where SQL Server will get deployed"
+  
+}
+
+
+variable "adminpassword" {
+  type = string
+  description = "Location of the SQL Server or other resources"
+
+}
+
+variable "adminloginusername" {
+    type = string
+    description = "Resource Group where SQL Server will get deployed"
+  
+}
+
+
+variable "adadminuserobject" {
+  type = string
+  description = "Location of the SQL Server or other resources"
+
+}
