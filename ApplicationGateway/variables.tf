1152bb972fd2ce54dbbfdfe60920786e0a57bb73
sauravbaldwin <41564194+sauravbaldwin@users.noreply.github.com>
2023-07-11 16:23:44 +0530

    Add files via upload

---
+++ ApplicationGateway/variables.tf
@@ -0,0 +1,41 @@
+variable "apgwname" {
+    type = string
+    description = "Name of the APGW"
+  
+}
+
+variable "vnetname" {
+    type = string
+    description = "Name of the APGW"
+  
+}
+
+variable "rgname" {
+    type = string
+    description = "Name of the APGW"
+  
+}
+
+variable "location" {
+    type = string
+    description = "Name of the APGW"
+  
+}
+
+variable "skuname" {
+    type = string
+    description = "Name of the APGW"
+  
+}
+
+variable "skutier" {
+    type = string
+    description = "Name of the APGW"
+  
+}
+
+variable "skucapacity" {
+    type = string
+    description = "Name of the APGW"
+  
+}
