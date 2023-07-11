7cdf3dd60235fbe8e0e551dcf5e998ac4d3a3f6a
sauravbaldwin <41564194+sauravbaldwin@users.noreply.github.com>
2023-07-11 16:26:30 +0530

    Update main.tf

--- NetwrokSecurityGroup/main.tf
+++ NetwrokSecurityGroup/main.tf
@@ -1,1 +1,19 @@
-
+resource "azurerm_network_security_group" "nsg" {
+  name                = "subnet1-nsg"
+  location            = var.location
+  resource_group_name = var.rgname
+}
+ 
+resource "azurerm_network_security_rule" "nsgrule" {
+  name                        = "test123"
+  priority                    = 100
+  direction                   = "Outbound"
+  access                      = "Allow"
+  protocol                    = "Tcp"
+  source_port_range           = "*"
+  destination_port_range      = "*"
+  source_address_prefix       = "*"
+  destination_address_prefix  = "*"
+  resource_group_name         = var.rgname
+  network_security_group_name = azurerm_network_security_group.nsg.name
+}
