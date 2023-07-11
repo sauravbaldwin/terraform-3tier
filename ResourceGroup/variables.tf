6e680492a45b5e6d050c950614e11711d4479f10
sauravbaldwin <41564194+sauravbaldwin@users.noreply.github.com>
2023-07-11 16:29:03 +0530

    Add files via upload

---
+++ ResourceGroup/variables.tf
@@ -0,0 +1,10 @@
+variable "rgname" {
+  type = string
+  description = "Name of the resource group"
+}
+
+variable "location" {
+  type = string
+  description = "Location of the Resource Group or other resources"
+}
+
