9b192fb61d455d6d9e43b98a9a730af55bf0934c
sauravbaldwin <41564194+sauravbaldwin@users.noreply.github.com>
2023-07-11 16:27:24 +0530

    Add files via upload

---
+++ NetwrokSecurityGroup/outputs.tf
@@ -0,0 +1,9 @@
+output "nsgname_out" {
+    value = azurerm_network_security_group.nsg.name
+  
+}
+
+output "nsgid_out" {
+    value = azurerm_network_security_group.nsg.id
+  
+}
---
+++ NetwrokSecurityGroup/variables.tf
@@ -0,0 +1,17 @@
+variable "location" {
+    type = string
+    description = "Location of the NSG or other Resource"
+  
+}
+
+
+variable "vnetname" {
+    type = string
+    description = "Name of the Vnet"
+}
+variable "rgname" {
+    type = string
+    description = "ResourceGroup Name where NSG will reside"
+  
+}
+
